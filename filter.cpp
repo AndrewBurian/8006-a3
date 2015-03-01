da3355060319b566121d1bbebab400b607ae8d77
JordanMarling <marling.jordan@gmail.com>
2015-03-01 15:10:18 -0800

    Added get_name to Filter class.

--- filter.cpp
+++ filter.cpp
@@ -10,3 +10,7 @@ void Filter::run(){
 void Filter::add_action(Action &action) {
 	
 }
+
+std::string Filter::get_name() {
+	return name;
+}
--- filter.h
+++ filter.h
@@ -6,11 +6,16 @@
 class Action;
 
 class Filter {
+private:
+	
+	std::string name;
+	
 public:
 	
 	Filter(std::string &name, std::string &logFilePath);
 	void run();
 	void add_action(Action &action);
+	std::string get_name();
 	
 };
 
