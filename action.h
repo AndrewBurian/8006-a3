dde61d975a568b7ede96b12ebc8771b2ec54c0ff
Andrew Burian <andrew@burian.ca>
2015-03-02 19:13:18 -0800

    Added action object logic
    Action object loads commands from file on create
    Act function does replacements and calls system

--- action.h
+++ action.h
@@ -2,15 +2,24 @@
 #define ACTION_H
 
 #include <string>
+#include <list>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <map>
+
+#define PATH "./action.d/"
+#define TYPE ".act"
 
 class Action{
 
 private:
   std::string _name;
+  std::list<std::string> _actions;
 
 public:
   Action(std::string &name);
-  void act(std::string &hostname);
+  void act(std::map<std::string, std::string> &replacements);
   std::string get_name();
 };
 
