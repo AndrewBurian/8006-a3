3f479c614f23ed87a35e00ec1122817b1a78db8e
Andrew Burian <andrew@burian.ca>
2015-03-04 20:51:44 -0800

    Added action undo with sleeping thread.

--- action.cpp
+++ action.cpp
@@ -14,9 +14,35 @@ Action::Action(std::string &name, int banTime) : _name(name), _banTime(banTime) 
 
   // read in the actions and add them
   std::string action;
+
+  // find the acts header
   while(std::getline(actionsFile, action)){
+    if(action == "[act]"){
+      break;
+    }
+  }
+
+  // read acts
+  while(std::getline(actionsFile, action)){
+    // skip comments and blanks
+    if(action.front() == '#' || action.front() == '\n'){
+      continue;
+    }
+    // if we're into unacts
+    if(action == "[unact]"){
+      break;
+    }
     _actions.push_back(action);
   }
+
+  // alread read unact
+  do{
+    // skip comments and blanks
+    if(action.front() == '#' || action.front() == '\n'){
+      continue;
+    }
+    _unacts.push_back(action);
+  }while(std::getline(actionsFile, action));
 
   // close the file
   actionsFile.close();
@@ -44,7 +70,37 @@ void Action::act(std::map<std::string, std::string> &replacements){
     // all replacements made, envoke action
     system(act.c_str());
 
+    // start the unacts
+    _threads.push_back(new std::thread(&Action::unact, this, replacements));
+
   }
+}
+
+void Action::unact(std::map<std::string, std::string> replacements){
+
+  sleep(_banTime);
+
+  // for each action to take
+  for(std::string act : _unacts){
+    // range based for loop gets copies
+
+    // for each replacement to make
+    for(std::map<std::string, std::string>::const_iterator it = replacements.begin();
+      it != replacements.end(); ++it){
+
+      // find the position of the target string, replace it with the replacement
+      size_t pos = act.find("<" + it->first + ">");
+      if(pos != std::string::npos){
+        act.replace(pos, it->first.length() + 2, it->second);
+      }
+
+    }
+
+    // all replacements made, envoke action
+    system(act.c_str());
+
+  }
+
 }
 
 std::string Action::get_name(){
--- action.h
+++ action.h
@@ -2,11 +2,13 @@
 #define ACTION_H
 
 #include <string>
-#include <list>
+#include <vector>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <map>
+#include <thread>
+#include <unistd.h>
 
 #define ACTION_PATH "./action.d/"
 #define ACTION_TYPE ".act"
@@ -16,7 +18,11 @@ class Action{
 private:
   std::string _name;
   int _banTime;
-  std::list<std::string> _actions;
+  std::vector<std::string> _actions;
+  std::vector<std::string> _unacts;
+  std::vector<std::thread*> _threads;
+
+  void unact(std::map<std::string, std::string> replacements);
 
 public:
   Action(std::string &name, int banTime);
