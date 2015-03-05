21e0d254e939bc4eb2310fddaa0c1faa58ece9fa
AndrewBurian <andrew@burian.ca>
2015-03-04 17:06:35 -0800

    Fixed things lost to revert and redid some file changes

--- filter.h
+++ filter.h
@@ -26,16 +26,19 @@ Revisions:
 #include <string>
 #include <vector>
 
+#define FILTER_PATH "./filter.d/"
+#define FILTER_TYPE ".fil"
+
 class Action;
 
 class Filter {
 private:
-	
+
 	struct filter_data {
 		regex_t expression;
 		std::vector<std::string> keywords;
 	};
-	
+
 	std::ifstream log_file;
 	std::string filter_name;
 	std::string log_file_name;
@@ -44,21 +47,21 @@ private:
 	std::map<std::string, std::string> filter_parameters;
 	std::map<std::string, std::vector<long>> attempts_by_hostname;
 	regex_t *datetime_expression;
-	
+
 	int period;
 	int attempts;
-	
+
 	void load_filter();
 	void check_log_line(std::string line);
-	
+
 public:
-	
+
 	Filter(std::string name, std::string logFile, int _period, int _attempts);
 	~Filter();
 	void run();
 	void add_action(Action &action);
 	std::string get_name();
-	
+
 };
 
 #endif
